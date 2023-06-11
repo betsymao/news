7f7339b82cb4338a1ffbc357d1c89ca5da21cbe1
Betsy <wdbetsy@gmail.com>
2023-06-11 11:59:13 +1000

    google  analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,10 +2,37 @@ import '@/styles/scss/main.scss';
 import { Fragment } from 'react';
 import Head from 'next/head';
 import Layout from '@/components/layout';
+import Script from 'next/script';
+
+
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-LGBDP9L3LP"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-LGBDP9L3LP');
+// </script>
 
 function App({ Component, pageProps }) {
   return (
     <Fragment>
+      <Script 
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy='afterInteractive'
+      />
+      <Script 
+        id='google-analytics-script'
+        strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
       <Head>
         <link rel="shortcut icon" href="/images/favicon.ico" />
       </Head>
